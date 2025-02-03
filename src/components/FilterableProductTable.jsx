8d853d237f741348f688e10c69203137100b93ae
Kelvin Costa <okelvincosta@gmail.com>
2025-02-03 18:32:12 -0300

    feat: Separate components

---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import ProductTable from './ProductTable';
+import SearchBar from './SearchBar';
+
+function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+  
+    return (
+      <div>
+        <SearchBar 
+          filterText={filterText} 
+          inStockOnly={inStockOnly} 
+          onFilterTextChange={setFilterText} 
+          onInStockOnlyChange={setInStockOnly} />
+        <ProductTable 
+          products={products} 
+          filterText={filterText}
+          inStockOnly={inStockOnly} />
+      </div>
+    );
+  }
+
+  export default FilterableProductTable;
